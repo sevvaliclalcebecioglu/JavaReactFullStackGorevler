d629f48e90f54bab6ec21effae0b51f6f190ad5f
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-04-10 21:51:59 +0300

    variables

--- variables/src/main/java/org/example/Main.java
+++ variables/src/main/java/org/example/Main.java
@@ -5,5 +5,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int ogrenciSayisi = 14;
+        String mesaj = "Öğrenci sayısı : ";
+
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
     }
 }
