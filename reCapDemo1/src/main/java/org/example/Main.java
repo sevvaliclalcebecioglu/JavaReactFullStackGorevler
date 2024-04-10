0039a7f2816bdb599f1f6c7f5650ca7c13bf6818
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-04-10 23:14:44 +0300

    en büyük sayı

--- reCapDemo1/src/main/java/org/example/Main.java
+++ reCapDemo1/src/main/java/org/example/Main.java
@@ -5,6 +5,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int sayi1 = 30;
+        int sayi2 = 25;
+        int sayi3 = 2;
+
+        // en büyük sayıyı "sayi1" olarak varsayarsak
+        int enBuyuk = sayi1;
+
+        if (enBuyuk < sayi2) {
+            enBuyuk = sayi2;
+        }
+
+        if (enBuyuk < sayi3) {
+            enBuyuk = sayi3;
+        }
+
+        System.out.println("En büyük : " + enBuyuk);
+
 
     }
 }
