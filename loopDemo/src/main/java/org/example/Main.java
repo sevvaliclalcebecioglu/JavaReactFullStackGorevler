e47f2a83b7e781016f0e1366eceb20f092e68358
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-04-11 10:11:28 +0300

    for döngüsü

--- loopDemo/src/main/java/org/example/Main.java
+++ loopDemo/src/main/java/org/example/Main.java
@@ -4,7 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //For
+        for (int i=2;i<10;i++){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
 
-
+        System.out.println("-------------------------------------");
     }
 }
